153c355d91b603d5bd63da64ccb871bc940a575d
Hannah Callison <hannahcallison1007@gmail.com>
2022-05-17 19:03:18 -0700

    adding project-2 to portfolio

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,20 @@
 var projectOne = document.getElementById("project-1")
 var projectTwo = document.getElementById("project-2")
 var projectThree = document.getElementById("project-3")
+var projectFour = document.getElementById("project-4")
 
 // Get the modal
 var modalOne = document.getElementById("myModalOne");
 var modalTwo = document.getElementById("myModalTwo");
 var modalThree = document.getElementById("myModalThree");
+var modalFour = document.getElementById("myModalFour");
 
 
 // Get the <span> element that closes the modal
 var spanOne = document.getElementsByClassName("close")[0];
 var spanTwo = document.getElementById("spanTwo");
 var spanThree = document.getElementById("spanThree");
+var spanFour = document.getElementById("spanFour");
 
 // When the user clicks on the button, open the modal
 projectOne.onclick = function () {
@@ -26,6 +29,10 @@ projectThree.onclick = function () {
     modalThree.style.display = "block";
 }
 
+projectFour.onclick = function () {
+    modalFour.style.display = "block";
+}
+
 
 // When the user clicks on <span> (x), close the modal
 spanOne.onclick = function () {
@@ -38,6 +45,10 @@ spanTwo.onclick = function () {
 
 spanThree.onclick = function () {
     modalThree.style.display = "none";
+}
+
+spanFour.onclick = function () {
+    modalFour.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
@@ -56,6 +67,12 @@ window.onclick = function (event) {
 window.onclick = function (event) {
     if (event.target == modalThree) {
         modalThree.style.display = "none";
+    }
+}
+
+window.onclick = function (event) {
+    if (event.target == modalFour) {
+        modalFour.style.display = "none";
     }
 }
 
